40227cca408fc9ab44f2239e373f6a4b0362ef86
iliya1984 <55304922+iliya1984@users.noreply.github.com>
2020-06-27 01:25:10 +0300

    web app rename

---
+++ BoltTest/BoltTest.Spa/DI/WebAppDIModule.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using BoltTest.Client.DI;
+using BoltTest.Core.DI;
+using BoltTest.Spa.Interfaces;
+using BoltTest.Spa.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoltTest.Spa.DI
+{
+    public class WebAppDIModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterModule<CoreDIModule>();
+            builder.RegisterModule<ClientDIModule>();
+
+            builder
+                .RegisterType<SearchManager>()
+                .As<ISearchMananger>();
+        }
+    }
+}
---
+++ BoltTest/BoltTest.Spa/Interfaces/ISearchMananger.cs
@@ -0,0 +1,13 @@
+﻿using BoltTest.Spa.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoltTest.Spa.Interfaces
+{
+    public interface ISearchMananger
+    {
+        Task<SearchResultViewModel> SearchAsync(string queryWord);
+    }
+}
